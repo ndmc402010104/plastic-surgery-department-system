2b1c3198edf0f7b51fa0175d94159a20309e922e
益昇 <ndmc402010104@gmail.com>
2026-06-06 15:43:55 +0800

    Release skhps v2.37.0-202606061543

--- 共用設定檔/EnvironmentFooter.js
+++ 共用設定檔/EnvironmentFooter.js
@@ -55,7 +55,7 @@
       shortLabel:'正式版',
       url:'https://skhps.jonaminz.com',
       apiUrl:'https://script.google.com/macros/s/AKfycbwbz8pXfU68j2aFeF_AaDmmG6Vco3JsPSw-PGyYeLu0AF3vCfzaZJQFOjORnwSw8Xp4/exec',
-      version:'v2.37.0-202606061132',
+      version:'v2.37.0-202606061543',
       type:'web'
     }
   };
@@ -890,6 +890,7 @@
     renderEnvironmentFooter();
   }
 })(typeof window !== 'undefined' ? window : this);
+
 
 
 
